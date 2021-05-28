c91588eb23f9b9ee74394d0a6901a8394f6e8bbf
Jeff <mx.jeferson.12@gmail.com>
2021-05-28 17:32:25 -0300

    make json extraction

--- app.py
+++ app.py
@@ -1,45 +1,33 @@
 from factory import getLinkedin
-from utils import JSONtoExcel, save_to_json, extract_link
+from utils import JSONtoExcel, save_to_json, extract_link, extract_json_data
 
 links = [
     "https://www.linkedin.com/in/fabio-seiki-ishitani-240a3625/",
     "https://www.linkedin.com/in/rafaela-rompatto-corr%C3%AAa/"
 ]
 
+profile_detail = []
+
 def main():
     print('> Iniciando crawler..')
     api = getLinkedin()
 
-    # for link in links:
-    data = dict()
+    # for link in links
     user = extract_link("https://www.linkedin.com/in/fabio-seiki-ishitani-240a3625/")
     
     print('> Extraindo perfíl')
     profile = api.get_profile(user)
 
-    data['Sumário'] = profile["summary"]
-    data['Name'] = profile['firstName'] + ' ' + profile['lastName']
-    data['Ocupação'] = profile["headline"]
-    data['Localização'] = profile["geoLocationName"] + ' ' + profile['locationName']
-    
-    company_detail = []
-    for experience in profile["experience"]:
-        print(experience.get("locationName"))
-        print(experience.get('companyName'))
-        print(experience.get("description"))
-        inicio = f"{experience['timePeriod']['startDate']['month']}/{experience['timePeriod']['startDate']['year']}"
+    data = extract_json_data(profile)
 
-        print(inicio)
+    print(data)
 
-        print(experience.get("title"))
-        print('\n')
-
-    # print(data)
-
-    # print('Escrevendo no arquivo...')
-    # save_to_json(profile, 'profile')
-    # print('Terminado!')
-    # JSONtoExcel('profile')
+    print('Escrevendo no arquivo...')
+    json_array = []
+    json_array.append(data)
+    save_to_json(json_array, 'data')
+    print('Terminado!')
+    JSONtoExcel('data')
     #  JSONJ
 
 if __name__ == "__main__":
