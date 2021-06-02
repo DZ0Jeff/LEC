32d9515fa9c6ee0ec2f4c261d2d213d77dd4760d
Jeff <mx.jeferson.12@gmail.com>
2021-06-02 09:42:53 -0300

    excel v6

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def main():
     for index, link in enumerate(links):
         print(f'{index} usuário')
         save_counter(index)
-        if index >= 201:
+        if index >= 251:
             print('[LPC]> Extraindo perfíl')
             user = extract_link(link)
             print(user)
